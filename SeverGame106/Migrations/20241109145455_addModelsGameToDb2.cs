baeac8fc02eb49f4f62bdb6cf2dbbad20662cb7b
nguyenvandat-pd11278 <ndat39625@gmail.com>
2024-11-10 12:23:55 +0700

    upload lan dau

---
+++ SeverGame106/Migrations/20241109145455_addModelsGameToDb2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeverGame106.Migrations
+{
+    /// <inheritdoc />
+    public partial class addModelsGameToDb2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LevelResults",
+                columns: table => new
+                {
+                    QuizResultId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LevelId = table.Column<int>(type: "int", nullable: false),
+                    Score = table.Column<int>(type: "int", nullable: false),
+                    CompletionDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LevelResults", x => x.QuizResultId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LevelResults");
+        }
+    }
+}
---
+++ SeverGame106/Models/GameLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeverGame106.Models
+{
+    public class GameLevel
+    {
+        [Key]
+        public int LevelId {  get; set; }
+        public string? title { get; set; }
+        public string? description { get; set; }
+    }
+}
